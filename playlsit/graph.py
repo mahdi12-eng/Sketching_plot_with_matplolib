e61abec041482aa9ad352c824653491716b661b2
Mahdi <hatamimahdi32@gmail.com>
2026-02-26 15:48:20 +0430

    using a little bit professional way in plot

---
+++ playlsit/graph.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import numpy as np
+x = np.array([0,10])
+y = np.array([0,10])
+
+plt.plot(x,y)
+
+
+x1 =np.array([1,2,3,4,5,6,7,8,9,10])
+y1 = np.array([4,9,15,10,14,15,15,21,3,11])
+plt.plot(x1,y1)
+plt.show()
---
+++ playlsit/second.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([1,2,3,4,5,6,7])
+y = np.array([3,0,9,3,2,8,1])
+plt.plot(x,y, marker ='4')
+plt.show()
